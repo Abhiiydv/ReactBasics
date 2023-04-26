0bd1839e51a2c300d9155688d0d1f3d00806cf8b
Abhishek <abhishekydv98@outlook.com>
2023-04-26 11:00:55 +0530

    added clear button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,11 +10,16 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText);
     }
+    const handleToClear = () => {
+        let newText = ('');
+        setText(newText);
+    }
     const handleOnChange = (event) => {
         setText(event.target.value);
        
     }
-    const [text, setText] = useState('Enter text here');
+    
+    const [text, setText] = useState('');
     // text="new text"; wrong way to update text//
     // setText("new updated text") correct way//    
 
@@ -25,8 +30,9 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary mx-3" onClick={handleToClick}>Convert to Lowercase</button>
+            <button className="btn btn-sm btn-primary mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-sm btn-primary mx-3" onClick={handleToClick}>Convert to Lowercase</button>
+            <button className="btn btn-sm btn-primary mx-3" onClick={handleToClear}>Clear Text</button>
         </div>
         <div className="container my-3">
             <h2>Text Summary : </h2>
