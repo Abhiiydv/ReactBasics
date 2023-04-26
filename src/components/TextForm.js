2a9a0c8ab4d488a8e17d62786d1eb436f1d7451a
Abhishek <abhishekydv98@outlook.com>
2023-04-26 10:30:08 +0530

    used setState and handled event to change text to Uppercase from input field

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
+
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+       
+    }
+    const [text, setText] = useState('Enter text here');
+    // text="new text"; wrong way to update text//
+    // setText("new updated text") correct way//    
+
     return (
         <div>
             <h2>{props.heading}</h2>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary">Convert to Uppercase</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
     )
 }
