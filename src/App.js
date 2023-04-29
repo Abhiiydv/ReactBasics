7ac08b0c6b698a4abd4ac29568dad24c355ec757
Abhishek <abhishekydv98@outlook.com>
2023-04-29 09:55:02 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -3,17 +3,40 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import { useState } from 'react';
+import {
+  BrowserRouter as Router, Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 function App() {
+
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+
+    }
+  }
+
   return (
     <>
 
 
-<Navbar title="TextUtils" aboutText="About"></Navbar>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
 
-<div className="container my-3">
-  <TextForm heading="Enter the text to analyze"></TextForm>
- {/* <About></About> */}
-</div>
+      <div className="container my-3">
+        <TextForm heading="Enter text to play with.." mode={mode} />
+        {/* <About></About> */}
+      </div>
+      <About></About>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,11 +6,11 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleToClick = () => {
+    const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
     }
-    const handleToClear = () => {
+    const handleClearClick = () => {
         let newText = ('');
         setText(newText);
     }
@@ -24,20 +24,24 @@ export default function TextForm(props) {
     // setText("new updated text") correct way//    
 
     return (
-        <>
-            <div>
-                <h2>{props.heading}</h2>
-                <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-                </div>
-                <button className="btn btn-sm btn-primary mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-sm btn-primary mx-3" onClick={handleToClick}>Convert to Lowercase</button>
-                <button className="btn btn-sm btn-primary mx-3" onClick={handleToClear}>Clear Text</button>
-            </div>
-            <div className="container my-3">
-                <h2>Text Summary : </h2>
-                <p>{text.split(" ").length} words and {text.length} characters.</p>
-            </div>
+        <> <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
+        <h1 className='mb-4'>{props.heading}</h1>
+        <div className="mb-3"> 
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+        </div>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+      
+    </div>
+           <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <h2>Your text summary</h2>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <h2 style={{color: props.mode==='dark'?'wheat':'black'}}>Preview</h2>
+            <p style={{color: props.mode==='dark'?'wheat':'black'}}>{text.length>0?text:"Nothing to preview!"}</p>
+        </div>
+           
         </>
 
     )
